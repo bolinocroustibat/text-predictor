e0c67f412b8a6547ccd64f4ba01fccc24556e176
Adrien Carpentier <moi@adriencarpentier.com>
2020-02-18 19:47:36 +0900

    Minor: linting with Black

--- text_predictor.py
+++ text_predictor.py
@@ -8,24 +8,27 @@ import numpy as np
 import time
 import matplotlib.pyplot as plt
 
-matplotlib.use('Agg')
+matplotlib.use("Agg")
 
 # Args
 if len(sys.argv) != 2:
-    print('Please select a dataset.')
-    print('Usage: python text_predictor.py <dataset>')
-    print('Available datasets: stupeflip.')
+    print("Please select a dataset.")
+    print("Usage: python text_predictor.py <dataset>")
+    print("Available datasets: stupeflip.")
     exit(1)
 else:
     dataset = sys.argv[1]
 
 # I/O
-data_dir = './data/' + dataset
-tensorboard_dir = data_dir + '/tensorboard/' + str(time.strftime('%Y-%m-%d_%H-%M-%S'))
-input_file = data_dir + '/input.txt'
-now = datetime.datetime.now()
-output_file = data_dir + '/output_{}-{}-{}_{}-{}.txt'.format(now.year, now.month, now.day, now.hour, now.minute)
-output = open(output_file, 'w')
+data_dir: str = "./data/" + dataset
+tensorboard_dir: str = data_dir + "/tensorboard/" + str(
+    time.strftime("%Y-%m-%d_%H-%M-%S")
+)
+input_file: str = data_dir + "/input.txt"
+output_file: str = "{data_dir}/output_{dt.year}-{dt.month}-{dt.day}_{dt.hour}-{dt.minute}.txt".format(
+    dt=datetime.datetime.now()
+)
+output = open(output_file, "w")
 output.close()
 
 # Hyperparams
@@ -43,7 +46,13 @@ LOGGING_FREQUENCY = 1000
 
 def rnn():
     data_provider = DataProvider(data_dir, BATCH_SIZE, SEQUENCE_LENGTH)
-    model = RNNModel(data_provider.vocabulary_size, batch_size=BATCH_SIZE, sequence_length=SEQUENCE_LENGTH, hidden_layer_size=HIDDEN_LAYER_SIZE, cells_size=CELLS_SIZE)
+    model = RNNModel(
+        data_provider.vocabulary_size,
+        batch_size=BATCH_SIZE,
+        sequence_length=SEQUENCE_LENGTH,
+        hidden_layer_size=HIDDEN_LAYER_SIZE,
+        cells_size=CELLS_SIZE,
+    )
 
     with tf.Session() as sess:
 
@@ -57,7 +66,9 @@ def rnn():
         smooth_losses = []
 
         while True:
-            sess.run(tf.assign(model.learning_rate, LEARNING_RATE * (DECAY_RATE ** epoch)))
+            sess.run(
+                tf.assign(model.learning_rate, LEARNING_RATE * (DECAY_RATE ** epoch))
+            )
             data_provider.reset_batch_pointer()
             state = sess.run(model.initial_state)
             for batch in range(data_provider.batches_size):
@@ -67,7 +78,9 @@ def rnn():
                     feed[c] = state[index].c
                     feed[h] = state[index].h
                 iteration = epoch * data_provider.batches_size + batch
-                summary, loss, state, _ = sess.run([summaries, model.cost, model.final_state, model.train_op], feed)
+                summary, loss, state, _ = sess.run(
+                    [summaries, model.cost, model.final_state, model.train_op], feed
+                )
                 writer.add_summary(summary, iteration)
                 temp_losses.append(loss)
 
@@ -78,18 +91,27 @@ def rnn():
                     smooth_loss = np.mean(temp_losses)
                     smooth_losses.append(smooth_loss)
                     temp_losses = []
-                    plot(smooth_losses, 'iterations (thousands)', 'loss')
-                    print(f'metric: iteration, value: {iteration}')
-                    print(f'metric: epoch, value: {epoch}')
-                    print(f'metric: loss, value: {smooth_loss}')
+                    plot(smooth_losses, "iterations (thousands)", "loss")
+                    print(f"metric: iteration, value: {iteration}")
+                    print(f"metric: epoch, value: {epoch}")
+                    print(f"metric: loss, value: {smooth_loss}")
             epoch += 1
 
 
 def sample_text(sess, data_provider, iteration):
-    model = RNNModel(data_provider.vocabulary_size, batch_size=1, sequence_length=1, hidden_layer_size=HIDDEN_LAYER_SIZE, cells_size=CELLS_SIZE, training=False)
-    text = model.sample(sess, data_provider.chars, data_provider.vocabulary, TEXT_SAMPLE_LENGTH).encode('utf-8')
-    with open(output_file, 'a') as output:
-        output.write(f'Iteration: {iteration}\n{text}\n\n')
+    model = RNNModel(
+        data_provider.vocabulary_size,
+        batch_size=1,
+        sequence_length=1,
+        hidden_layer_size=HIDDEN_LAYER_SIZE,
+        cells_size=CELLS_SIZE,
+        training=False,
+    )
+    text = model.sample(
+        sess, data_provider.chars, data_provider.vocabulary, TEXT_SAMPLE_LENGTH
+    ).encode("utf-8")
+    with open(output_file, "a") as output:
+        output.write(f"Iteration: {iteration}\n{text}\n\n")
 
 
 def plot(data, x_label, y_label):
@@ -97,16 +119,16 @@ def plot(data, x_label, y_label):
     plt.title(dataset)
     plt.xlabel(x_label)
     plt.ylabel(y_label)
-    plt.savefig(data_dir + '/' + y_label + '.png', bbox_inches='tight')
+    plt.savefig(data_dir + "/" + y_label + ".png", bbox_inches="tight")
     plt.close()
 
 
-if __name__ == '__main__':
-    print(f'Selected dataset: {dataset}')
-    print(f'Batch size: {BATCH_SIZE}')
-    print(f'Sequence length: {SEQUENCE_LENGTH}')
-    print(f'Learning rate: {LEARNING_RATE}')
-    print(f'Decay rate: f{DECAY_RATE}')
-    print(f'Hidden layer size: {HIDDEN_LAYER_SIZE}')
-    print(f'Cells size: {CELLS_SIZE}')
+if __name__ == "__main__":
+    print(f"Selected dataset: {dataset}")
+    print(f"Batch size: {BATCH_SIZE}")
+    print(f"Sequence length: {SEQUENCE_LENGTH}")
+    print(f"Learning rate: {LEARNING_RATE}")
+    print(f"Decay rate: f{DECAY_RATE}")
+    print(f"Hidden layer size: {HIDDEN_LAYER_SIZE}")
+    print(f"Cells size: {CELLS_SIZE}")
     rnn()
